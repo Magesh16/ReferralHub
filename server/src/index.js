f469576d67fd85cfc72a2acb12264a1fdbb97ef1
Priyadharshan <91417662+Priyadharshan0903@users.noreply.github.com>
2026-03-22 14:13:23 +0530

    Added cors

--- server/src/index.js
+++ server/src/index.js
@@ -22,7 +22,12 @@ const app = express();
 //   origin: ['http://localhost:5173', 'http://localhost:3000', 'https://referralhub-client.onrender.com/'],
 //   credentials: true,
 // }));
-app.use(cors());
+app.use(cors({
+  origin: "https://referralhub-client.onrender.com",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
