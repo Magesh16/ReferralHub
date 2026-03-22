c02b198b9c3b6019fba8adf3703b7d2703f016b8
Priyadharshan <91417662+Priyadharshan0903@users.noreply.github.com>
2026-03-22 13:57:44 +0530

    whitelisted all ips

--- server/src/index.js
+++ server/src/index.js
@@ -18,10 +18,11 @@ const resumeMatchRoutes = require('./routes/resumeMatch.routes');
 const app = express();
 
 // ── Middleware ──
-app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'https://referralhub-client.onrender.com/'],
-  credentials: true,
-}));
+// app.use(cors({
+//   origin: ['http://localhost:5173', 'http://localhost:3000', 'https://referralhub-client.onrender.com/'],
+//   credentials: true,
+// }));
+app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
